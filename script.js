05b95ab8d0a3693fa559a5eb9ad71ac5723783dc
chrtho16 <chrtho16@vgs.nfk.no>
2024-04-09 14:57:28 +0200

    changed

---
+++ script.js
@@ -0,0 +1,11 @@
+var score = 0
+ 
+var uranium = document.getElementById('click_image')
+var EL_score = document.getElementById('score')
+ 
+function click_main() {
+    score = score + 1
+    EL_score.innerHTML = score
+}
+ 
+uranium.addEventListener('click', click_main)
