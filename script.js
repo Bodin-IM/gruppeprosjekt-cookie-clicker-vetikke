7894dc391a07461b4258591b47f17a32e66a7d49
chrtho16 <chrtho16@vgs.nfk.no>
2024-04-15 13:56:22 +0200

    mæv

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var EL_score = document.getElementById('score')
  
 function click_main() {
     score = score + 1
-    EL_score.innerHTML = score
+    EL_score.innerHTML = "Mined / Score: " + score
 }
  
 uranium.addEventListener('click', click_main)
