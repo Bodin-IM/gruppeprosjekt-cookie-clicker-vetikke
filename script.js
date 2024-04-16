78ece5a27396a6a0ead137e05aeccfa183371e3f
chrtho16 <chrtho16@vgs.nfk.no>
2024-04-16 15:19:43 +0200

    update

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 var score = 0
- 
+var tool_eff = 1
+
 var uranium = document.getElementById('click_image')
 var EL_score = document.getElementById('score')
- 
+// Purchase Items
+var upgrade_tool = document.getElementById('upgrade_tool')
+var upgrade_tool_text = document.getElementById('upgrade_tool_text')
+
 function click_main() {
-    score = score + 1
+    score = score + Math.round(1 * tool_eff)
     EL_score.innerHTML = "Mined / Score: " + score
 }
  
 uranium.addEventListener('click', click_main)
+
+function purchase_upgrade_tool() {
+    if (score >= 15) {
+        score = score - 15
+        tool_eff = tool_eff + 0.25
+        console.log("Tool Eff.: " + tool_eff)
+    }
+    EL_score.innerHTML = "Mined / Score: " + score
+    upgrade_tool_text.innerHTML = "Upgrade Tool Efficiency (" + tool_eff + ")" + "<br>Cost: 15"
+}
+
+upgrade_tool.addEventListener('click', purchase_upgrade_tool)
